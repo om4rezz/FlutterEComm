99d141612212723f4193a026fb81dd3e97b93d79
MostafaElBadawy1 <81087849+MostafaElBadawy1@users.noreply.github.com>
2023-11-09 01:44:52 +0200

    update item design

--- lib/view/home/components/live_pricing/metal_price_card.dart
+++ lib/view/home/components/live_pricing/metal_price_card.dart
@@ -15,7 +15,7 @@ class MetalPriceCard extends StatelessWidget {
       child: ClipRRect(
         borderRadius: BorderRadius.circular(10),
         child: Container(
-          width: 180,
+          width: MediaQuery.of(context).size.width * 0.4,
           height: 130,
           color: metalItem.color,
           child: Column(
