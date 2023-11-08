d6687f78ae8be1cb0f8437d0b28e36f584eb4bb5
MostafaElBadawy1 <81087849+MostafaElBadawy1@users.noreply.github.com>
2023-11-08 21:07:03 +0200

    fix tapping on popular category and popular products navigation

--- lib/view/home/components/popular_category/popular_category_card.dart
+++ lib/view/home/components/popular_category/popular_category_card.dart
@@ -2,6 +2,8 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:ecomm_app/model/category.dart';
 import 'package:flutter/material.dart';
 import 'package:shimmer/shimmer.dart';
+import 'package:ecomm_app/controller/controllers.dart';
+import 'dart:ui';
 
 class PopularCategoryCard extends StatelessWidget {
   final Category category;
@@ -13,77 +15,88 @@ class PopularCategoryCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.fromLTRB(10, 10, 5, 10),
-      child: CachedNetworkImage(
-        imageUrl: category.photo ??
-            "https://hassanmokh.pythonanywhere.com/media/album/Banner/Banner_605e915d02f943daa50305766f37b7e2.png",
-        imageBuilder: (context, imageProvider) => Material(
-          elevation: 8,
-          shadowColor: Colors.grey.shade300,
-          borderRadius: BorderRadius.circular(10),
-          child: Container(
-              width: 270,
-              height: 140,
-              decoration: BoxDecoration(
-                color: Colors.grey.shade300,
-                borderRadius: BorderRadius.circular(10),
-                image: DecorationImage(image: imageProvider, fit: BoxFit.cover),
-              ),
-              child: Stack(
-                children: [
-                  Positioned(
-                    bottom: 0,
-                    child: Padding(
-                      padding: const EdgeInsets.all(10.0),
-                      child: Text(
-                        category.name,
-                        style: const TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
+    return InkWell(
+      onTap: () {
+        dashboardController.updateIndex(1);
+        productController.searchTextEditController.text =
+        'cat: ${category.name}';
+        productController.searchVal.value =
+        'cat: ${category.name}';
+        productController.getProductByCategory(
+            id:category.id);
+      },
+      child: Padding(
+        padding: EdgeInsets.fromLTRB(10, 10, 5, 10),
+        child: CachedNetworkImage(
+          imageUrl: category.photo ??
+              "https://hassanmokh.pythonanywhere.com/media/album/Banner/Banner_605e915d02f943daa50305766f37b7e2.png",
+          imageBuilder: (context, imageProvider) => Material(
+            elevation: 8,
+            shadowColor: Colors.grey.shade300,
+            borderRadius: BorderRadius.circular(10),
+            child: Container(
+                width: 270,
+                height: 140,
+                decoration: BoxDecoration(
+                  color: Colors.grey.shade300,
+                  borderRadius: BorderRadius.circular(10),
+                  image: DecorationImage(image: imageProvider, fit: BoxFit.cover),
+                ),
+                child: Stack(
+                  children: [
+                    Positioned(
+                      bottom: 0,
+                      child: Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Text(
+                          category.name,
+                          style: const TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
                         ),
                       ),
-                    ),
-                  )
-                ],
-              )),
-        ),
-        placeholder: (context, url) => Material(
-          elevation: 8,
-          shadowColor: Colors.grey.shade300,
-          borderRadius: BorderRadius.circular(10),
-          child: Shimmer.fromColors(
-            baseColor: Colors.grey.shade300,
-            highlightColor: Colors.white,
-            child: Container(
-              width: 270,
-              height: 140,
-              decoration: BoxDecoration(
-                color: Colors.grey.shade300,
-                borderRadius: BorderRadius.circular(10),
+                    )
+                  ],
+                )),
+          ),
+          placeholder: (context, url) => Material(
+            elevation: 8,
+            shadowColor: Colors.grey.shade300,
+            borderRadius: BorderRadius.circular(10),
+            child: Shimmer.fromColors(
+              baseColor: Colors.grey.shade300,
+              highlightColor: Colors.white,
+              child: Container(
+                width: 270,
+                height: 140,
+                decoration: BoxDecoration(
+                  color: Colors.grey.shade300,
+                  borderRadius: BorderRadius.circular(10),
+                ),
               ),
             ),
           ),
-        ),
-        errorWidget: (context, url, error) => Material(
-          elevation: 8,
-          shadowColor: Colors.grey.shade300,
-          borderRadius: BorderRadius.circular(10),
-          child: Shimmer.fromColors(
-            baseColor: Colors.grey.shade300,
-            highlightColor: Colors.white,
-            child: Container(
-              width: 270,
-              height: 140,
-              decoration: BoxDecoration(
-                color: Colors.grey.shade300,
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: const Center(
-                child: Icon(
-                  Icons.error_outline,
-                  color: Colors.grey,
+          errorWidget: (context, url, error) => Material(
+            elevation: 8,
+            shadowColor: Colors.grey.shade300,
+            borderRadius: BorderRadius.circular(10),
+            child: Shimmer.fromColors(
+              baseColor: Colors.grey.shade300,
+              highlightColor: Colors.white,
+              child: Container(
+                width: 270,
+                height: 140,
+                decoration: BoxDecoration(
+                  color: Colors.grey.shade300,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: const Center(
+                  child: Icon(
+                    Icons.error_outline,
+                    color: Colors.grey,
+                  ),
                 ),
               ),
             ),
--- lib/view/home/components/popular_product/popular_product_card.dart
+++ lib/view/home/components/popular_product/popular_product_card.dart
@@ -1,4 +1,5 @@
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:ecomm_app/view/product_details/product_details_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:ecomm_app/constants.dart';
 import 'package:ecomm_app/model/product.dart';
@@ -14,52 +15,61 @@ class PopularProductCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.fromLTRB(10, 10, 5, 10),
-      child: Material(
-        elevation: 8,
-        shadowColor: Colors.grey.shade300,
-        borderRadius: BorderRadius.circular(10),
-        child: Container(
-          margin: const EdgeInsets.all(10),
-          width: 120,
-          child: Column(children: [
-            AspectRatio(
-              aspectRatio: 0.9,
-              child: CachedNetworkImage(
-                imageUrl: product.images?.firstOrNull ??
-                    "https://hassanmokh.pythonanywhere.com/media/album/Banner/Banner_605e915d02f943daa50305766f37b7e2.png",
-                placeholder: (context, url) => Shimmer.fromColors(
-                  baseColor: Colors.grey.shade300,
-                  highlightColor: Colors.white,
-                  child: Container(
-                    color: Colors.grey,
-                    padding: const EdgeInsets.all(15),
-                    margin: const EdgeInsets.symmetric(
-                      horizontal: 25,
+    return InkWell(
+        onTap: () {
+          FocusScope.of(context).requestFocus(FocusNode());
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => ProductDetailsScreen(product: product)));
+        },
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(10, 10, 5, 10),
+        child: Material(
+          elevation: 8,
+          shadowColor: Colors.grey.shade300,
+          borderRadius: BorderRadius.circular(10),
+          child: Container(
+            margin: const EdgeInsets.all(10),
+            width: 120,
+            child: Column(children: [
+              AspectRatio(
+                aspectRatio: 0.9,
+                child: CachedNetworkImage(
+                  imageUrl: product.images?.firstOrNull ??
+                      "https://hassanmokh.pythonanywhere.com/media/album/Banner/Banner_605e915d02f943daa50305766f37b7e2.png",
+                  placeholder: (context, url) => Shimmer.fromColors(
+                    baseColor: Colors.grey.shade300,
+                    highlightColor: Colors.white,
+                    child: Container(
+                      color: Colors.grey,
+                      padding: const EdgeInsets.all(15),
+                      margin: const EdgeInsets.symmetric(
+                        horizontal: 25,
+                      ),
+                    ),
+                  ),
+                  errorWidget: (context, url, error) => const Center(
+                    child: Icon(
+                      Icons.error_outline,
+                      color: Colors.grey,
                     ),
                   ),
                 ),
-                errorWidget: (context, url, error) => const Center(
-                  child: Icon(
-                    Icons.error_outline,
-                    color: Colors.grey,
-                  ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 10, bottom: 10),
+                child: Text(
+                  product.title,
+                  style: const TextStyle(color: Colors.black, fontSize: 14),
+                  maxLines: 1,
                 ),
-              ),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 10, bottom: 10),
-              child: Text(
-                product.title,
-                style: const TextStyle(color: Colors.black, fontSize: 14),
-                maxLines: 1,
-              ),
-            )
-          ]),
+              )
+            ]),
+          ),
         ),
       ),
     );
